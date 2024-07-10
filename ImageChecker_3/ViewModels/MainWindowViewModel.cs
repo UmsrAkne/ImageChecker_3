d2782af741948c6c14c113766d4a10ec1e42cf81
Akn <sibata486@gmail.com>
2024-07-11 00:45:34 +0900

    init /

---
+++ ImageChecker_3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace ImageChecker_3.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "ImageCheckerV3";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
