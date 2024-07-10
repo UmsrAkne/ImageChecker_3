3f713255c876b5c13791e3714ca2639d422cf80f
Akn <sibata486@gmail.com>
2024-07-11 06:34:49 +0900

    add feat / プレビュー画像を縦横に動かす機能を実装

--- ImageChecker_3/Models/Images/PreviewContainer.cs
+++ ImageChecker_3/Models/Images/PreviewContainer.cs
@@ -7,19 +7,36 @@ namespace ImageChecker_3.Models.Images
     public class PreviewContainer : BindableBase
     {
         private double previewScale = 0.5;
+        private Rect screenRect = new Rect(0, 0, 1280, 720);
 
         public ObservableCollection<ImageWrapper> ImageWrappers { get; set; } = new ();
 
         /// <summary>
         /// 実際のプレイ時の画面のサイズを表します。
         /// </summary>
-        public Rect ScreenRect { get; set; } = new Rect(0, 0, 1280, 720);
+        public Rect ScreenRect
+        {
+            get => screenRect;
+            set => SetProperty(ref screenRect, value);
+        }
 
         /// <summary>
         /// 実際の画面のサイズに対する、プレビュー領域のサイズ比です。
         /// 例えば、このプロパティの値が 0.5 ならば、画面サイズに対してプレビュー領域が半分の大きさとなります。
         /// </summary>
         public double PreviewScale { get => previewScale; set => SetProperty(ref previewScale, value); }
+
+        public double X
+        {
+            get => ScreenRect.X;
+            set => ScreenRect = ScreenRect with { X = value, };
+        }
+
+        public double Y
+        {
+            get => ScreenRect.Y;
+            set => ScreenRect = ScreenRect with { Y = value, };
+        }
 
         public void SetImageWrappers(ImageWrapper a, ImageWrapper b, ImageWrapper c, ImageWrapper d)
         {
