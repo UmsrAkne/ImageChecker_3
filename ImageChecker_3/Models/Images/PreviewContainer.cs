a95337ea801185cf013a30139c015db04168bd66
Akn <sibata486@gmail.com>
2024-07-11 06:00:04 +0900

    del / PreviewContainer.PreviewRect プロパティを削除。ScreenRect に置き換え。
    MultiValueConverter の使用により、プロパティを分ける必要がなくなったので統合した。

--- ImageChecker_3/Models/Images/PreviewContainer.cs
+++ ImageChecker_3/Models/Images/PreviewContainer.cs
@@ -10,10 +10,15 @@ namespace ImageChecker_3.Models.Images
 
         public ObservableCollection<ImageWrapper> ImageWrappers { get; set; } = new ();
 
-        public Rect ScreenSize { get; set; }
+        /// <summary>
+        /// 実際のプレイ時の画面のサイズを表します。
+        /// </summary>
+        public Rect ScreenRect { get; set; } = new Rect(0, 0, 1280, 720);
 
-        public Rect PreviewRect { get; set; } = new Rect(0, 0, 1280, 720);
-
+        /// <summary>
+        /// 実際の画面のサイズに対する、プレビュー領域のサイズ比です。
+        /// 例えば、このプロパティの値が 0.5 ならば、画面サイズに対してプレビュー領域が半分の大きさとなります。
+        /// </summary>
         public double PreviewScale { get => previewScale; set => SetProperty(ref previewScale, value); }
 
         public void SetImageWrappers(ImageWrapper a, ImageWrapper b, ImageWrapper c, ImageWrapper d)
