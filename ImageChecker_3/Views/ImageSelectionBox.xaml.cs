5f43790c6483cc85d5b04e715165c387bc28390b
Akn <sibata486@gmail.com>
2024-07-13 01:47:29 +0900

    add / ImageSelectionBox.xaml , xaml.cs を追加。
    コピーしてきただけなので、コンパイル可能だが、まだ使用できない。

---
+++ ImageChecker_3/Views/ImageSelectionBox.xaml.cs
@@ -0,0 +1,10 @@
+namespace ImageChecker_3.Views
+{
+    public partial class ImageSelectionBox
+    {
+        public ImageSelectionBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
