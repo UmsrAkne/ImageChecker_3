2a5e646f73c34fddc3b1ddb3d5d90b8a0e4a5ed4
Akn <sibata486@gmail.com>
2024-07-12 17:39:43 +0900

    add feat / ディレクトリ内の画像ファイルを読み込む処理を実装。

--- ImageChecker_3/Models/Images/ImageWrapperProvider.cs
+++ ImageChecker_3/Models/Images/ImageWrapperProvider.cs
@@ -1,16 +1,24 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ImageChecker_3.Models.Images
 {
     public class ImageWrapperProvider : IImageWrapperProvider
     {
+        private List<ImageWrapper> imageWrappers = new ();
+
         public List<ImageWrapper> GetImageWrappers(char keyChar)
         {
-            return new List<ImageWrapper>();
+            return imageWrappers.Where(w => w.ImageFileInfo.KeyChar == keyChar).ToList();
         }
 
         public void Load(string directoryPath)
         {
+            imageWrappers = Directory.GetFiles(directoryPath)
+                    .Where(p => Path.GetExtension(p) == ".png")
+                    .Select(p => new ImageWrapper(new ImageFileInfo(p)))
+                    .ToList();
         }
     }
 }
