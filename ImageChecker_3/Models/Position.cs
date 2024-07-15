9aee28c6f0ddbdb45fd7be9edc4265bfbbeeef99
Akn <sibata486@gmail.com>
2024-07-15 17:16:08 +0900

    add / 位置を表す enum を追加。

---
+++ ImageChecker_3/Models/Position.cs
@@ -0,0 +1,14 @@
+namespace ImageChecker_3.Models
+{
+    public enum Position
+    {
+        Left,
+        BottomLeft,
+        Bottom,
+        BottomRight,
+        Right,
+        TopRight,
+        Top,
+        TopLeft,
+    }
+}
