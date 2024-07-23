5146c20bdae2fd77c10fe98ecbfc57bf007494fb
Akn <sibata486@gmail.com>
2024-07-24 05:26:30 +0900

    Update version number.

--- ImageChecker_3/Models/TitleBarText.cs
+++ ImageChecker_3/Models/TitleBarText.cs
@@ -30,7 +30,7 @@ namespace ImageChecker_3.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20240722" + "a";
+            Version = "20240724" + "a";
         }
 
         [Conditional("DEBUG")]
