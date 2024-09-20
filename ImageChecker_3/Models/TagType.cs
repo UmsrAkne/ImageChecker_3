62b0a5a68930356b414968b8d87d3d0e4c869133
Akn <sibata486@gmail.com>
2024-09-20 18:23:23 +0900

    add comment / TagType の各値にドキュメンテーションコメントを追加。

--- ImageChecker_3/Models/TagType.cs
+++ ImageChecker_3/Models/TagType.cs
@@ -2,14 +2,29 @@ namespace ImageChecker_3.Models
 {
     public enum TagType
     {
+        /// <summary>
+        /// タイプなし（デフォルト）
+        /// </summary>
         NoType = 0,
 
+        /// <summary>
+        /// Image タグ
+        /// </summary>
         Image,
 
+        /// <summary>
+        /// Draw タグ
+        /// </summary>
         Draw,
 
+        /// <summary>
+        /// AnimationImage タグ
+        /// </summary>
         AnimationImage,
 
+        /// <summary>
+        /// AnimationDraw タグ
+        /// </summary>
         AnimationDraw,
     }
 }
