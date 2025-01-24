b817f44db3d32f31cd25ba307e0c389f979680dd
Akn <sibata486@gmail.com>
2025-01-24 18:17:52 +0900

    refactoring / タグオブジェクトを生成するための共通処理を LoadImageTag から分離した。

--- ImageChecker_3/Models/TagParser.cs
+++ ImageChecker_3/Models/TagParser.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using ImageChecker_3.Models.Tags;
 
 namespace ImageChecker_3.Models
 {
-    public class TagParser
+    public static class TagParser
     {
         private static string tagPattern = @"^<(\w+)(?:\s+(\w+)=""([^""]+)"")*\s*/>$";
 
@@ -13,40 +12,43 @@ namespace ImageChecker_3.Models
         {
             var result = new ImageTag();
 
-            // 正規表現でタグの形式を検証
-            if (Regex.IsMatch(input, tagPattern))
+            if (!Regex.IsMatch(input, tagPattern))
             {
-                Console.WriteLine("正しいタグ形式です。");
-
-                // 属性を抽出する正規表現
-                const string attributePattern = @"(\w+)=""([^""]+)""";
-                var matches = Regex.Matches(input, attributePattern);
-
-                // 各属性を格納する辞書
-                var attributes = new Dictionary<string, string>();
-
-                foreach (Match match in matches)
-                {
-                    var key = match.Groups[1].Value;
-                    var value = match.Groups[2].Value;
-                    attributes[key] = value;
-                }
-
-                // 属性値の変数に格納（適切な型に変換）
-                result.A = attributes.GetValueOrDefault("a", string.Empty);
-                result.B = attributes.GetValueOrDefault("b", string.Empty);
-                result.C = attributes.GetValueOrDefault("c", string.Empty);
-                result.D = attributes.GetValueOrDefault("d", string.Empty);
-                result.X = int.Parse(attributes.GetValueOrDefault("x", "0"));
-                result.Y = int.Parse(attributes.GetValueOrDefault("y", "0"));
-                result.Scale = float.Parse(attributes.GetValueOrDefault("scale", "1.0"));
+                System.Diagnostics.Debug.WriteLine($"タグの形式が正しくありません (TagParser : 17)");
+                return result;
             }
-            else
-            {
-                System.Diagnostics.Debug.WriteLine($"タグの形式が正しくありません (TagParser : 47)");
-            }
+
+            var attributes = ExtractAttributes(input);
+
+            // 属性値の変数に格納（適切な型に変換）
+            result.A = attributes.GetValueOrDefault("a", string.Empty);
+            result.B = attributes.GetValueOrDefault("b", string.Empty);
+            result.C = attributes.GetValueOrDefault("c", string.Empty);
+            result.D = attributes.GetValueOrDefault("d", string.Empty);
+            result.X = int.Parse(attributes.GetValueOrDefault("x", "0"));
+            result.Y = int.Parse(attributes.GetValueOrDefault("y", "0"));
+            result.Scale = double.Parse(attributes.GetValueOrDefault("scale", "1.0"));
 
             return result;
+        }
+
+        private static Dictionary<string, string> ExtractAttributes(string input)
+        {
+            // 属性を抽出する正規表現
+            const string attributePattern = @"(\w+)=""([^""]+)""";
+            var matches = Regex.Matches(input, attributePattern);
+
+            // 各属性を格納する辞書
+            var attributes = new Dictionary<string, string>();
+
+            foreach (Match match in matches)
+            {
+                var key = match.Groups[1].Value;
+                var value = match.Groups[2].Value;
+                attributes[key] = value;
+            }
+
+            return attributes;
         }
     }
 }
