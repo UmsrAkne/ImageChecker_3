43ce039e875f20f3d77c12d2d3c074485caeac3e
Akn <sibata486@gmail.com>
2025-02-07 20:08:34 +0900

    add feat / TagLoadPage でタグのテキストを読み込み、画面に表示する処理を実装した。

--- ImageChecker_3/ViewModels/TagLoadPageViewModel.cs
+++ ImageChecker_3/ViewModels/TagLoadPageViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using ImageChecker_3.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
@@ -8,9 +9,16 @@ namespace ImageChecker_3.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class TagLoadPageViewModel : BindableBase, IDialogAware
     {
+        private string inputText;
+        private string parsedResult;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public string InputText { get => inputText; set => SetProperty(ref inputText, value); }
+
+        public string ParsedResult { get => parsedResult; private set => SetProperty(ref parsedResult, value); }
 
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
@@ -19,7 +27,20 @@ namespace ImageChecker_3.ViewModels
 
         public DelegateCommand ParseTagTextCommand => new DelegateCommand(() =>
         {
-            
+            if (InputText.StartsWith("<image"))
+            {
+                ParsedResult = TagParser.LoadImageTag(InputText).ToString();
+            }
+
+            if (InputText.StartsWith("<draw"))
+            {
+                ParsedResult = TagParser.LoadDrawTag(InputText).ToString();
+            }
+
+            if (InputText.StartsWith("<anime") && InputText.Contains(@"name=""slide"""))
+            {
+                ParsedResult = TagParser.LoadSlideTag(InputText).ToString();
+            }
         });
 
         public bool CanCloseDialog() => true;
