4fe06500c3ae983db9461c369b0e97843d38f895
UmsrAkne <sibata486@gmail.com>
2025-04-08 01:27:30 +0900

    refactoring / ConvertBack の実装を Binding.DoNothing に書き換えた。

--- ImageChecker_3/Views/Converters/RoundDoubleConverter.cs
+++ ImageChecker_3/Views/Converters/RoundDoubleConverter.cs
@@ -27,7 +27,7 @@ namespace ImageChecker_3.Views.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return Binding.DoNothing;
         }
     }
 }
