d023b434a82f8229555b1cc21198fe0b4940cd43
UmsrAkne <sibata486@gmail.com>
2025-04-08 01:18:41 +0900

    improve ui / double の性質に起因する拡大率の表示の不具合に対処。
    余分な小数部を削るためのコンバーターを実装し、ビューに設置した。

---
+++ ImageChecker_3/Views/Converters/RoundDoubleConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ImageChecker_3.Views.Converters
+{
+    public class RoundDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var d = (double)value;
+                var isNegative = d < 0;
+                d *= 100;
+                d = Math.Round(Math.Abs(d)) / 100;
+                if (isNegative)
+                {
+                    d *= -1;
+                }
+
+                return d.ToString("F2");
+            }
+
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
